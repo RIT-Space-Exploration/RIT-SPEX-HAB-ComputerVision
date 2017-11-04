095aaf18a68d00dae59fac727dfef066b7b99f6c
Trevor Sherrard <sherrardtr@gmail.com>
2017-11-03 23:47:31 -0400

    Fix bugs in main.cpp, Add verbosity to line detection code

--- src/detectLines.cpp
+++ src/detectLines.cpp
@@ -1,3 +1,11 @@
+/* Author: Trevor Sherrard
+ * Date: November 2017
+ * Purpose: This file contains the implementation 
+ *          of houghLinesAndDraw. This function detects 
+ *          lines and draws detected lines onto the
+ *          Mat dest.
+ */
+
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -7,18 +15,61 @@
 using namespace cv;
 using namespace std;
 
+/* houghLinesAndDraw attempts to find lines
+ * in Mat src. Any lines found in the image that meet
+ * the threshold criteria are drawn onto Mat dest.
+ * 
+ * The vector of Vec4i containing endpoints of lines
+ * detected is returned.
+ */
 vector<Vec4i> houghLinesAndDraw(Mat src, Mat &dest)
 {
     Mat cdest;
-    Canny(src, cdest, 50, 200, 3);
-    cvtColor(cdest, cdest, COLOR_GRAY2BGR);
+    int LThresh = 50;
+    int HThresh = 200;
+    int KernelSize = 3;
+    int rhoRes = 1;
+    double thetaRes = CV_PI/180;
+    int IntersectThresh = 50;
+    int minLineLen = 50;
+    int maxLineGap = 10;
 
+    // Convert src to grayscale store in cdest
+    cvtColor(src, cdest, COLOR_GRAY2BGR);
+
+    // Apply blur to reduce noise with a 3x3 kernel
+    blur(cdest, cdest, Size(3,3));    
+
+    /* src -> source image
+     * cdest -> canny image
+     * LThresh -> Lower gradient threshold
+     * HThresh -> Higher gradient threshold
+     * KernelSize -> Internal Sobel filter kernel size
+     */
+    Canny(cdest, cdest, LThresh, HThresh, KernelSize);
+
+    // make empty vector of Vec4i for detected line endpoints.
     vector<Vec4i> lines;
-    HoughLinesP(cdest, lines, 1, CV_PI/180, 50, 50, 10);
+
+    /* cdest -> image to dected lines in.
+     * lines -> vector of Vec4i to store detected line endpoints.
+     * rhoRes -> resolution of Rho in pixels.
+     * thetaRes -> resolution of theta in radians.
+     * IntersectThresh -> Aproximation line intersection threshold.
+     * minLineLen -> Minimum line length needed for detection.
+     * maxLineGap -> Max gap between two points on a line 
+     *               to still be considered in the same line.
+     */
+    HoughLinesP(cdest, lines, rhoRes, thetaRes, IntersectThresh, 
+        minLineLen, maxLineGap);
+
+    // loop through detected lines and draw onto dest Mat
     for(int i = 0; i < lines.size(); i++)
     {
         Vec4i l = lines[i];
         line(dest, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
     }
+
+    // return list of detected lines
     return lines;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/* Author: Trevor Sherrard
+ * Date: November 2017
+ * Purpose: This program serves to supply horizon 
+ *          detection and frame capture/save functionality
+ *          to the RIT SPEX HAB system
+ */
+
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -15,16 +22,18 @@ char* frameIndxCam2;
 
 unsigned long frameCount = 0;
 
+// usecase help message
 void printHelpMessage()
 {
     cout << "---------Program CLI commands---------" << endl;
-    cout << "./executable <camIndex1> <camIndex2>" << endl;
+    cout << "./RIT_SPEX_Horizon <camIndex1> <camIndex2>" << endl;
     cout << "" << endl;
     cout << "if no arguments are given, default camIndexes are 1 and 2." << endl;
 }
 
 int main(int argc, char** argv)
 {
+    // check for non-default indexes
     if(argc > 2)
     {
         char* VidIndex1 = argv[1];
@@ -33,9 +42,11 @@ int main(int argc, char** argv)
         VidIndexInt2 = atoi(VidIndex2);
     }
 
+    // Initialize Camera captures
     VideoCapture cap1(VidIndexInt1);
     VideoCapture cap2(VidIndexInt2);
 
+    // make sure camera streams open independently
     if(!cap1.isOpened())
     {
         cout << "failed to open camera 1 stream !" << endl;
@@ -48,20 +59,25 @@ int main(int argc, char** argv)
         printHelpMessage();
         return -1;
     }
+    
     while(true)
     {
+        // Init capture matricies.
         Mat curFrameCam1;
         Mat curFrameCam2;
 
         vector<Vec4i> VecCam1;
         vector<Vec4i> VecCam2;
+
+        // try to capture a frame
         bool successCam1 = cap1.read(curFrameCam1);
         bool successCam2 = cap2.read(curFrameCam2);
         frameCount++;
 
-        sprintf(frameIndxCam1, "../../images/Cam1_frameIndex%lu", frameCount);
-        sprintf(frameIndxCam2, "../../images/Cam2_frameIndex%lu", frameCount);
+        sprintf(frameIndxCam1, "../../images/Cam1_frameIndex%lu.png", frameCount);
+        sprintf(frameIndxCam2, "../../images/Cam2_frameIndex%lu.png", frameCount);
 
+        // save off captures if sucessful
         if(successCam1)
         {
             imwrite(frameIndxCam1, curFrameCam1);
@@ -71,24 +87,30 @@ int main(int argc, char** argv)
             imwrite(frameIndxCam2, curFrameCam2);
         }
 
+        // Init matricies for detected horizon
         Mat destFrameCam1;
         Mat destFrameCam2;
 
+        // attempt to find horizons
         VecCam1 = houghLinesAndDraw(curFrameCam1, destFrameCam1);
         VecCam2 = houghLinesAndDraw(curFrameCam2, destFrameCam2);
 
+        // Save off frame with detected lines drawn on.
         if(VecCam1.size() != 0)
         {
-            sprintf(frameIndxCam1, "../../images/LinesCam1_frameIndex%lu", frameCount);
+            sprintf(frameIndxCam1, "../../images/LinesCam1_frameIndex%lu.png", frameCount);
             imwrite(frameIndxCam1, destFrameCam1);
         }
         if(VecCam2.size() != 0)
         {
-            sprintf(frameIndxCam2, "../../images/LinesCam2_frameIndex%lu", frameCount);
+            sprintf(frameIndxCam2, "../../images/LinesCam2_frameIndex%lu.png", frameCount);
             imwrite(frameIndxCam2, destFrameCam2);
         }
 
-        char keyStroke = (char) waitKey(1000);
+        // wait two seconds before looping again
+        char keyStroke = (char) waitKey(2000);
+        
+        // break out if escape is pressed
         if(keyStroke == 27)
         {
             break;
